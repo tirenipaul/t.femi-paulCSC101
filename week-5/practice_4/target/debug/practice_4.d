a2d327a3f2383317385f2e67e4edeca1c8145dbe
Tireni <tirenipaul@gmail.com>
2022-11-11 17:37:44 +0100

    (My fifth commit)

---
+++ week-5/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\Tireni\Documents\t.femi-paulCSC101\week-5\practice_4\target\debug\practice_4.exe: C:\Users\Tireni\Documents\t.femi-paulCSC101\week-5\practice_4\src\main.rs
---
+++ week-5/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\Tireni\Documents\t.femi-paulCSC101\week-5\practice_8\target\debug\practice_8.exe: C:\Users\Tireni\Documents\t.femi-paulCSC101\week-5\practice_8\src\main.rs
